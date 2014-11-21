ce107f3851c09209fd459d21aea54115fe46a90d
Bunlong <bunlong.van@gmail.com>
2014-11-21 15:01:41 +0700

    test model labor

--- app/models/labor.rb
+++ app/models/labor.rb
@@ -1,2 +1,8 @@
 class Labor < ActiveRecord::Base
+  include UuidHelper
+
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :position_uuid, length: { maximum: 36 }, presence: true
+  validates :labor_project_uuid, length: { maximum: 36 }
+  validates :labor_subordinate, length: { maximum: 36 }
 end
