dd779d9e0078c46485dcc26024a5f83133e7f8b1
Teo Paocheak <teopaocheak@yahoo.com>
2014-11-25 17:04:10 +0700

    Created Warehouse model and test

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,2 +1,7 @@
 class Warehouse < ActiveRecord::Base
+  include UuidHelper
+
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :labor_uuid, length: {maximum: 36}
+  validates :warehouse_type_uuid, length: {maximum: 36}
 end
