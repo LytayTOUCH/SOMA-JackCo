4a4792c2e85e71149b2be96759a9ee24c49dec8f
Teo Paocheak <teopaocheak@yahoo.com>
2014-11-27 11:42:37 +0700

    Updating User Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
+
 
   private
   def after_sign_in_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # devise_for :users, controllers: { registrations: "registrations" }
 
+  
+
   devise_scope :user do
     get '/' => 'devise/sessions#new'
   end
