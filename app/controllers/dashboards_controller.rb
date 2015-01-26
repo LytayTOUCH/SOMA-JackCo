2f1da7cf10d3fab6c915c7113c7be0b8fe2daca1
Teo Paocheak <teopaocheak@yahoo.com>
2015-01-26 12:02:46 +0700

    Continued with Chart

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,30 +1,29 @@
 class DashboardsController < ApplicationController
   def index
-    @line_chart = Gchart.pie(:data => [20, 10,40], :labels => "Pie Chart", :size => '300x300')
 
-    data = [
-      {
-        name: "Fantasy & Sci Fi", 
-        data: [["2010", "10"], ["2020", "16"], ["2030", "28"]]
-      },
-      {
-        name: "Romance", 
-        data: [["2010", "24"], ["2020", "22"], ["2030", "19"]]
-      },
-      {
-        name: "Mystery/Crime", 
-        data: [["2010", "20"], ["2020", "23"], ["2030", "29"]]
-      }
-    ]
+    data = [['1997',10],['1998',20],['1999',40]]
+
+    @labels = Role.pluck(:label)
+
+    @line_chart = Gchart.pie(data: [60,30,50], title: 'Line chart', labels: @labels, size: '400x300')
 
     # data = [
-    #   ['Year', 'Sales', 'Expenses'],
-    #   ['2004',  1000,      400],
-    #   ['2005',  1170,      460],
-    #   ['2006',  660,       1120],
-    #   ['2007',  1030,      540]
-    # ]  
+    #   {
+    #     name: "Fantasy & Sci Fi", 
+    #     data: [["2010", "10"], ["2020", "16"], ["2030", "28"]]
+    #   },
+    #   {
+    #     name: "Romance", 
+    #     data: [["2010", "24"], ["2020", "22"], ["2030", "19"]]
+    #   },
+    #   {
+    #     name: "Mystery/Crime", 
+    #     data: [["2010", "20"], ["2020", "23"], ["2030", "29"]]
+    #   }
+    # ]
 
-    @bar_chart = Gchart.bar(:data => [20,30,80,60], :bar_width_and_spacing => '25,6')
+    data1 = [20, 60, 30]
+
+    @bar_chart = Gchart.bar(data: data1, bar_width_and_spacing: '35,20', title: 'Bar chart', labels: ["January", "Feburary", "March"], legend: ['courbe 1','courbe 2','courbe 3'], axis_with_label: ['J', 'F', 'M'], width: '500')
   end
 end
