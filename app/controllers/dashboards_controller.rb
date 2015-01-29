be2577b3ce57a3035e9d126cb645107c519d7bac
Teo Paocheak <teopaocheak@yahoo.com>
2015-01-29 11:33:00 +0700

    Added a Bar Chart

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,35 +6,11 @@ class DashboardsController < ApplicationController
 
     @labels = TestingChart.pluck(:name, :amount)
     
-    # @labels =[
-    #   ["Officer", 5],
-    #   ["IT Admin", 6],
-    #   ["Cleaner", 3],
-    #   ["Guard", 7],
-    #   ["Cook", 15],
-    #   ["Table Runner", 25],
-    #   ["Table Washer", 65]
-    # ]
     respond_with do |format|
       format.json {render :json => @labels}
     end
 
     @line_chart = Gchart.pie(data: [60,30,50], title: 'Line chart', labels: @labels, size: '400x300')
-
-    # data = [
-    #   {
-    #     name: "Fantasy & Sci Fi", 
-    #     data: [["2010", "10"], ["2020", "16"], ["2030", "28"]]
-    #   },
-    #   {
-    #     name: "Romance", 
-    #     data: [["2010", "24"], ["2020", "22"], ["2030", "19"]]
-    #   },
-    #   {
-    #     name: "Mystery/Crime", 
-    #     data: [["2010", "20"], ["2020", "23"], ["2030", "29"]]
-    #   }
-    # ]
 
     data1 = [20, 60, 30]
 
