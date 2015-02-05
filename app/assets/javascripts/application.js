09c699753c2362d1fc9aa50f5d63f032f7872613
Lytay TOUCH <l.touch@cl-tag.com>
2015-02-05 18:11:30 +0700

    Add navigator menu in to application layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
 //= require jquery
-//= require fullcalendar
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap
 //= require nprogress
 //= require nprogress-turbolinks
-//= require bootstrap/modal
-//= require bootstrap/scrollspy
-//= require bootstrap/dropdown
-//= require select2
-//= require fields
-//= require moment
-//= require calendars
-//= require bootstrap-datetimepicker
-//= require dashboards
+//= require turbolinks
+//= require_tree .
 
 $(document).ready(function() {
-  NProgress.start();
+	NProgress.configure({
+	  showSpinner: false,
+	});
+	NProgress.start();
 });
