4a1623bb1dc82aa1dbef5fd24ef650c35780ce27
Lytay TOUCH <l.touch@cl-tag.com>
2015-02-06 09:02:33 +0700

    Update conflict code on Google Map

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
   def google_map_api_drawing
     javascript_include_tag 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=drawing'
+  end
+  def google_chart_api
     javascript_include_tag 'https://www.google.com/jsapi'
   end
 end
