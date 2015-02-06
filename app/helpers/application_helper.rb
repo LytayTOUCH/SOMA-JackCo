2d01ea2c09e4dfd72af0c3dde145c9126dbf42e7
Lytay TOUCH <l.touch@cl-tag.com>
2015-02-06 09:20:20 +0700

    Update google api

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
   def google_map_api_drawing
     javascript_include_tag 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=drawing'
-    javascript_include_tag 'https://www.google.com/jsapi'
   end
   def google_chart_api
     javascript_include_tag 'https://www.google.com/jsapi'
