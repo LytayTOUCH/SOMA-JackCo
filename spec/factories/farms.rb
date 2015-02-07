5d32fd6bb0422f7f9b863f3b79cc694533a144e0
Lytay TOUCH <l.touch@cl-tag.com>
2015-02-07 16:05:42 +0700

    Add model farm

---
+++ spec/factories/farms.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :farm do
+    name "MyString"
+    location "MyString"
+    latlong_farm "MyString"
+  end
+end
