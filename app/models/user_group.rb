80f40e8099ab739dc7cb5e8cea02047d345d2e35
Teo Paocheak <teopaocheak@yahoo.com>
2015-02-10 14:35:16 +0700

    Nothing

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,7 +1,11 @@
 class UserGroup < ActiveRecord::Base
   include UuidHelper
 
+  attr_reader :resource_tokens
+  before_create :resource_tokens
+
   has_many :users
+  
   has_many :permissions, foreign_key: :user_group_id 
   has_many :resources, through: :permissions
   
