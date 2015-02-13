9cd02b6cfa912099da9291b060216a4700d280c2
Teo Paocheak <teopaocheak@yahoo.com>
2015-02-13 09:20:00 +0700

    Creating spec and editing migration for permission

--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
-describe Permission do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Permission, 'validation' do
+  
 end
+
+describe Permission, 'column-specification' do
+  it { should have_db_column(:name).of_type(:string).with_options(length: { maximum: 50 }) }
+  it { should have_db_column(:user_group_id).of_type(:string).with_options(length: { maximum: 36 }) }
+  it { should have_db_column(:resource_id).of_type(:string).with_options(length: { maximum: 36 }) }
+  it { should have_db_column(:access_full).of_type(:boolean) }
+  it { should have_db_column(:access_list).of_type(:boolean) }
+  it { should have_db_column(:access_create).of_type(:boolean) }
+  it { should have_db_column(:access_update).of_type(:boolean) }
+  it { should have_db_column(:access_delete).of_type(:boolean) }
+end  
