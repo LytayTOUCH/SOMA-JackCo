874f2cebee868facee6609ea10f47178f5b5e432
sopheak <sesopheak5@gmail.com>
2015-02-20 13:47:02 +0700

    Add Farm in Dashboard

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -3,4 +3,6 @@ class Farm < ActiveRecord::Base
   has_many :blocks
   has_many :planting_projects, through: :blocks
   validates :name, presence: true
+
+  scope :uuid, -> uuid_f { where(:uuid => uuid_f) }
 end
