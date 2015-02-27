24c19349af004ab5f3c198ab164eee4c2c716688
sopheak <sesopheak5@gmail.com>
2015-02-27 17:12:05 +0700

    Update Migrate File

--- db/migrate/20150227030543_add_column_user_group_id_and_remove_role.rb
+++ db/migrate/20150227030543_add_column_user_group_id_and_remove_role.rb
@@ -1,6 +1,12 @@
 class AddColumnUserGroupIdAndRemoveRole < ActiveRecord::Migration
   def change
-    add_column :users, :user_group_id, :string, limit: 36, null: false
-    remove_column :users, :role
+  	if column_exists? :users, :role
+	  remove_column :users, :role
+	end
+  	
+  	unless column_exists? :users, :user_group_id
+		add_column :users, :user_group_id, :string, limit: 36, null: false
+	end
+    
   end
 end
