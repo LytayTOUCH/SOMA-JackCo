6ff93060f9001956c2e41982bfd557992e907cc6
Reaksmey <r.reaksmey@gmail.com>
2015-03-03 13:35:59 +0700

    Remove TransactionModel from the system

--- app/models/transaction_status.rb
+++
@@ -1,6 +0,0 @@
-class TransactionStatus < ActiveRecord::Base
-  include UuidHelper
-
-  validates :name, length: { maximum: 50 }, presence: true
-  scope :find_by_transaction_name, -> name { where("name like ?", "%#{name}%") }
-end
