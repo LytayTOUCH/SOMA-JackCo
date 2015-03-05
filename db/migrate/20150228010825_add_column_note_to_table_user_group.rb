bdb4ca7409cab612bdbad0ecb345b7fec643b1b7
Reaksmey <r.reaksmey@gmail.com>
2015-03-05 15:07:15 +0700

    check column exist first in paocheak's migration

--- db/migrate/20150228010825_add_column_note_to_table_user_group.rb
+++ db/migrate/20150228010825_add_column_note_to_table_user_group.rb
@@ -1,5 +1,7 @@
 class AddColumnNoteToTableUserGroup < ActiveRecord::Migration
   def change
-    add_column :user_groups, :note, :text
+    unless column_exists? :user_groups, :note
+      add_column :user_groups, :note, :text
+    end
   end
 end
