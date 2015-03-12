fe5692ac5381f6c310e86119877a351be236f4a1
Teo Paocheak <teopaocheak@yahoo.com>
2015-03-12 10:45:22 +0700

    Inserting data to Table Warehouse Production Amount

--- app/models/production.rb
+++ app/models/production.rb
@@ -1,8 +1,14 @@
 class Production < ActiveRecord::Base
   include UuidHelper
 
+  attr_reader :warehouse_tokens
+  before_create :warehouse_tokens
+
   belongs_to :planting_project, foreign_key: :planting_project_id
   belongs_to :unit_of_measurement, foreign_key: :uom_id
+
+  has_many :warehouse_production_amounts, foreign_key: :production_id 
+  has_many :warehouses, through: :warehouse_production_amounts
 
   validates :status, length: { maximum: 20 }, presence: true
   validates :planting_project_id, length: {maximum: 36}, presence: true
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -6,6 +6,9 @@ class Warehouse < ActiveRecord::Base
   has_many :warehouse_material_amounts, foreign_key: :warehouse_uuid
   has_many :materials, through: :warehouse_material_amounts
 
+  has_many :warehouse_production_amounts, foreign_key: :warehouse_id
+  has_many :productions, through: :warehouse_production_amounts
+
   validates :name, length: { maximum: 50 }, presence: true
 
   validates :warehouse_type_uuid, length: {maximum: 36}, presence: true
