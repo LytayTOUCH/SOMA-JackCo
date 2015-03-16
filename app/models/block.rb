517c60e7ef71a8ebd1794c04cec500f9b49101dd
Lytay TOUCH <l.touch@cl-tag.com>
2015-03-16 10:55:46 +0700

    Modify form input farm using seperate add and edit form in farm page

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,6 +2,9 @@ class Block < ActiveRecord::Base
   include UuidHelper
   belongs_to :farm
   belongs_to :planting_project
-
+  validates :name, presence: true
+  validates :surface, presence: true
+  validates :tree_amount, presence: true
+  validates :shape_lat_long, presence: true
   scope :farm_id, -> uuid_f { where(:farm_id => uuid_f) }
 end
