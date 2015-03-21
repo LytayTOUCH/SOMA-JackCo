701b25ee3c90ec1b4c013f70aa0c47f05270c28a
Reaksmey <r.reaksmey@gmail.com>
2015-03-21 18:26:36 +0700

    implement list of material & production adjustment history

--- app/controllers/material_adjustments_controller.rb
+++ app/controllers/material_adjustments_controller.rb
@@ -1,4 +1,19 @@
 class MaterialAdjustmentsController < ApplicationController
+  def index
+    central_uuid = WarehouseType.find_by_name("Central Warehouse").uuid 
+    project_uuid = WarehouseType.find_by_name("Project Warehouse").uuid
+    fertilizer_uuid = WarehouseType.find_by_name("Fertilizer Warehouse").uuid
+    @warehouses = Warehouse.where("warehouse_type_uuid = ? and active = ? OR warehouse_type_uuid = ? and active = ? OR warehouse_type_uuid = ? and active = ?", central_uuid, true, project_uuid, true, fertilizer_uuid, true)
+    
+    if params[:warehouse] and params[:warehouse][:uuid] and params[:warehouse][:uuid] != "" and !params[:warehouse][:uuid].nil?
+      @selected_warehouse = Warehouse.find_by_uuid(params[:warehouse][:uuid])
+      @material_adjustments = MaterialAdjustmentDecorator.new(MaterialAdjustment.find_by_warehouse(params[:warehouse][:uuid]).page(params[:page]).per(session[:item_per_page]))
+    else
+      @selected_warehouse = Warehouse.new
+      @material_adjustments = MaterialAdjustmentDecorator.new(MaterialAdjustment.order(created_at: :desc).page(params[:page]).per(session[:item_per_page]))
+    end
+  end
+  
   def new
     warehouse_material_amount = WarehouseMaterialAmount.find(params[:id])
     
