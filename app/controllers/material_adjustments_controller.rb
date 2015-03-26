2c5eb01b56d8baccdee8fb3fa4ad003896424d13
Reaksmey <r.reaksmey@gmail.com>
2015-03-26 14:54:35 +0700

    change list style, and fix group error in production in stock & material in stock page

--- app/controllers/material_adjustments_controller.rb
+++ app/controllers/material_adjustments_controller.rb
@@ -1,6 +1,6 @@
 class MaterialAdjustmentsController < ApplicationController
   def index
-    @material_adjustments = MaterialAdjustment.order(created_at: :desc)
+    @material_adjustments = MaterialAdjustment.joins(:warehouse_material_amount).order("warehouse_material_amounts.warehouse_uuid, material_adjustments.adjust_date desc")
   end
   
   def new
--- app/controllers/production_adjustments_controller.rb
+++ app/controllers/production_adjustments_controller.rb
@@ -1,6 +1,6 @@
 class ProductionAdjustmentsController < ApplicationController
   def index
-    @production_adjustments = ProductionAdjustment.order(created_at: :desc)
+    @production_adjustments = ProductionAdjustment.joins(:warehouse_production_amount).order("warehouse_production_amounts.warehouse_id, production_adjustments.adjust_date desc")
   end
   
   def new
