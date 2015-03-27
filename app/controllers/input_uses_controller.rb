0dd85fa85f3bcf8785b78fe2390e861dcfa50007
sopheak <sesopheak5@gmail.com>
2015-03-27 09:27:40 +0700

    Input use page

--- app/controllers/input_uses_controller.rb
+++ app/controllers/input_uses_controller.rb
@@ -2,5 +2,12 @@ class InputUsesController < ApplicationController
   def index
   	@input_tasks = InputTask.where("start_date = ? AND end_date = ?", params[:start_date], params[:end_date])
 
+  	@input_tasks.each do |in_task|
+  		@start_date = in_task.start_date
+  		@end_date = in_task.end_date
+  	end
+
+  	@material_category = MaterialCategory.all
+
   end
 end
