4712386a343905411dd103a7c952dc1d3d6eb195
Reaksmey <r.reaksmey@gmail.com>
2015-03-30 13:34:32 +0700

    change adjustment of material and production, from redirect to open a popup instad

--- app/controllers/warehouse_material_amounts_controller.rb
+++ app/controllers/warehouse_material_amounts_controller.rb
@@ -13,4 +13,29 @@ class WarehouseMaterialAmountsController < ApplicationController
       puts e
     end
   end
+  
+  def edit
+    begin
+      @warehouse_material_amount = WarehouseMaterialAmount.find(params[:id])
+    rescue Exception => e
+      puts e
+    end
+  end
+
+  def update
+    adjust_date = Date.today
+    wha_id = @warehouse_material_amount.uuid
+    old_amount = @warehouse_material_amount.amount
+    new_amount = params[:warehouse_material_amount][:amount]
+    user_id = current_user.uuid
+    user_name = User.find(current_user.uuid).email
+    note = params[:material_note]
+    
+    if @warehouse_material_amount.update(amount: params[:warehouse_material_amount][:amount])
+      MaterialAdjustment.create(adjust_date: adjust_date,  warehouse_material_amount_id: wha_id, old_amount: old_amount, new_amount: new_amount, user_id: user_id, user_name: user_name, note: note)
+      redirect_to warehouse_material_amounts_path+"?wha_id="+wha_id
+    else
+      flash[:notice] = "Stock can not be adjusted"
+    end
+  end
 end
--- app/controllers/warehouse_production_amounts_controller.rb
+++ app/controllers/warehouse_production_amounts_controller.rb
@@ -12,4 +12,29 @@ class WarehouseProductionAmountsController < ApplicationController
       puts e
     end
   end
+  
+  def edit
+    begin
+      @warehouse_production_amount = WarehouseProductionAmount.find(params[:id])
+    rescue Exception => e
+      puts e
+    end
+  end
+
+  def update
+    adjust_date = Date.today
+    wha_id = @warehouse_production_amount.uuid
+    old_amount = @warehouse_production_amount.amount
+    new_amount = params[:warehouse_production_amount][:amount]
+    user_id = current_user.uuid
+    user_name = User.find(current_user.uuid).email
+    note = params[:production_note]
+    
+    if @warehouse_production_amount.update(amount: params[:warehouse_production_amount][:amount])
+      ProductionAdjustment.create(adjust_date: adjust_date,  warehouse_production_amount_id: wha_id, old_amount: old_amount, new_amount: new_amount, user_id: user_id, user_name: user_name, note: note)
+      redirect_to warehouse_production_amounts_path+"?wha_id="+wha_id
+    else
+      flash[:notice] = "Stock can not be adjusted"
+    end
+  end
 end
