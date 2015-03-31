a1936633b81fc52570883580e2d7863c2f58bae2
Reaksmey <r.reaksmey@gmail.com>
2015-03-31 15:32:06 +0700

    add image to machinery

--- app/models/machinery.rb
+++ app/models/machinery.rb
@@ -1,6 +1,9 @@
 class Machinery < ActiveRecord::Base
   include UuidHelper
   
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
   belongs_to :machinery_type, foreign_key: :machinery_type_id
   belongs_to :warehouse, foreign_key: :warehouse_id
   has_many :input_tasks, foreign_key: :machinery_id
