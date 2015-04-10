9c332be63d790f70f1ec65aeca47981c32728130
Reaksmey <r.reaksmey@gmail.com>
2015-04-10 18:33:44 +0700

    add validation to stock in page

--- app/models/stock_in.rb
+++ app/models/stock_in.rb
@@ -9,5 +9,13 @@ class StockIn < ActiveRecord::Base
   validates :material_id, length: {maximum: 36}, presence: true
   validates :labor_id, length: {maximum: 36}, presence: true
   validates :reference_number, length: {maximum: 30}, presence: true
-
+  validates :stock_in_date, presence: true
+  validates :amount, presence: true
+  validate :amount_must_greater_than_zero
+  
+  def amount_must_greater_than_zero
+    if material_id.present? && amount.present? && amount <= 0
+      errors.add(:amount, "must be grater than zero")
+    end
+  end
 end
