00526d486d32336b6d4b2c16eb29966afd1cb3d3
Lytay TOUCH <l.touch@cl-tag.com>
2015-04-18 08:52:51 +0700

    Comment out of zone, area and block relation in models

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,10 +1,11 @@
 class Farm < ActiveRecord::Base
   include UuidHelper
-  has_many :zones
+  # has_many :zones
 
-  has_many :areas, through: :zones
+  # has_many :areas, through: :zones
 
-  has_many :blocks, through: :areas
+  # has_many :blocks, through: :areas
+  has_many :blocks
   
   has_many :planting_projects, through: :blocks
   has_many :plan_farms
