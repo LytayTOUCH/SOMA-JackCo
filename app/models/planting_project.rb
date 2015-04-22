4c47c09de8ee65bbcc5ded8b4e7d6d8c0d6675ed
Lytay TOUCH <l.touch@cl-tag.com>
2015-04-22 15:51:22 +0700

    Put productional tree amount on block detail and modify zone and area relationship model

--- app/models/planting_project.rb
+++ app/models/planting_project.rb
@@ -3,17 +3,13 @@ class PlantingProject < ActiveRecord::Base
   
   has_many :blocks, foreign_key: :planting_project_id
   has_many :farms, through: :blocks
-  has_many :areas 
-
   has_many :input_tasks, through: :blocks
-  
   has_many :productions
-  validates :name, length: { maximum: 50 }, presence: true
-
   has_many :production_stages
   has_many :output_tasks, foreign_key: :planting_project_id
-  
   has_many :warehouse_production_amounts, through: :productions
+
+  validates :name, length: { maximum: 50 }, presence: true
 
   scope :find_by_project_name, -> name { where("name like ?", "%#{name}%") }
 
