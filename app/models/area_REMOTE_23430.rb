cf1d834dc181fa813b960bac8afeade12db651b3
Lytay TOUCH <l.touch@cl-tag.com>
2015-05-04 14:02:51 +0700

    Revome area backup uknown files

--- app/models/area_REMOTE_23430.rb
+++
@@ -1,7 +0,0 @@
-class Area < ActiveRecord::Base
-  include UuidHelper
-  has_many :blocks, foreign_key: :area_id
-  has_many :planting_projects, through: :blocks
-  belongs_to :zone
-  belongs_to :farm
-end
