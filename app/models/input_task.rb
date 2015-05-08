cd319827eddda0e3c15ff85cd95ef2aeeafaff20
sopheak <sesopheak5@gmail.com>
2015-05-08 08:21:03 +0700

    Update Input Task Validation

--- app/models/input_task.rb
+++ app/models/input_task.rb
@@ -1,44 +1,44 @@
 class InputTask < ActiveRecord::Base
-	include UuidHelper
+  include UuidHelper
 
-	belongs_to :block, foreign_key: :block_id
-	belongs_to :labor, foreign_key: :labor_id
-	belongs_to :planting_project
+  belongs_to :block, foreign_key: :block_id
+  belongs_to :labor, foreign_key: :labor_id
+  belongs_to :planting_project
 
-	has_many :input_use_materials, foreign_key: :input_id
-	has_many :materials, through: :input_use_materials
+  has_many :input_use_materials, foreign_key: :input_id
+  has_many :materials, through: :input_use_materials
 
-  	has_many :input_use_machineries, foreign_key: :input_id
-  	has_many :machineries, through: :input_use_machineries
+  has_many :input_use_machineries, foreign_key: :input_id
+  has_many :machineries, through: :input_use_machineries
 
-  	has_many :input_use_equipments, foreign_key: :input_id
-  	has_many :equipments, through: :input_use_equipments
+  has_many :input_use_equipments, foreign_key: :input_id
+  has_many :equipments, through: :input_use_equipments
 
-	validates :name, length: { maximum: 50 }, presence: true
-	validates :start_date, presence: true
-	validates :end_date, presence: true
-	validates :tree_amount, presence: true
-	validates :reference_number, presence: true
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :tree_amount, presence: true
+  validates :reference_number, presence: true
 
-  	validates :block_id, length: {maximum: 36}, presence: true
-  	validates :labor_id, length: {maximum: 36}, presence: true
-  	validates :created_by, length: {maximum: 36}, presence: true
-  	validates :planting_project_id, length: {maximum: 36}, presence: true
-  	
-  	scope :find_by_name, -> name { where("name like ?", "%#{name}%") }
-  	scope :planting_project_id, -> uuid_f { joins(:block).where("blocks.planting_project_id=?", uuid_f) }
+  validates :block_id, length: {maximum: 36}, presence: true
+  validates :labor_id, length: {maximum: 36}, presence: true
+  validates :created_by, length: {maximum: 36}, presence: true
+  validates :planting_project_id, length: {maximum: 36}, presence: true
+  
+  scope :find_by_name, -> name { where("name like ?", "%#{name}%") }
+  scope :planting_project_id, -> uuid_f { joins(:block).where("blocks.planting_project_id=?", uuid_f) }
 
-  	scope :start_date, -> start_date, end_date {
-		if start_date.present? || end_date.present?
-			if start_date.blank?
-				where("start_date <= ?", end_date ) 
-			elsif end_date.blank?
-				where("start_date >= ?", start_date ) 
-			elsif start_date.present? && end_date.present?
-				where("start_date >= ? AND start_date <= ?", start_date, end_date ) 
-			else
-			end
-		end
-
-	}
+  scope :start_date, -> start_date, end_date {
+    if start_date.present? || end_date.present?
+      if start_date.blank?
+        where("start_date <= ?", end_date ) 
+      elsif end_date.blank?
+        where("start_date >= ?", start_date ) 
+      elsif start_date.present? && end_date.present?
+        where("start_date >= ? AND start_date <= ?", start_date, end_date ) 
+      else
+      end
+    end
+  }
+  
 end
