a4b1f4abd2e0e71e3f24765026bade2b06156f1c
Teo Paocheak <teopaocheak@yahoo.com>
2015-05-16 11:51:42 +0700

    Testing and Fixing errors in Access Level

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,11 +8,13 @@ class Ability
       can :manage, :all
     else
       # =================== Manager =======================
-      can :read, [Labor, Position, PlantingProject, MachineryType, Machinery, EquipmentType, Equipment, MaterialAdjustment, WarehouseProductionAmount, StockIn, WarehouseItemTransaction, InputTask, OutputTask, Farm, Block, PlanFarm, ProductionPlan] if user.user_group.name == "Manager"
+      can :read, [Labor, Position, PlantingProject, MachineryType, Machinery, EquipmentType, Equipment, MaterialAdjustment, WarehouseProductionAmount, StockIn, WarehouseItemTransaction, InputTask, OutputTask, Farm, Block, Log, PlanFarm, ProductionPlan, ProductionStandard] if user.user_group.name == "Manager"
       
       can :manage, [Warehouse, Material, WarehouseMaterialReceived] if user.user_group.name == "Manager"
       
       can [:edit_profile, :update_profile], User if user.user_group.name == "Manager"
+
+      can :index, :production_plan_report if user.user_group.name == "Manager"
       
       cannot :index, :production_list if user.user_group.name == "Manager"
 
@@ -20,13 +22,15 @@ class Ability
         
         
       # =================== Project Leader =======================
-      can :read, [Labor, Position, MaterialAdjustment, Log, PlanFarm, ProductionPlan] if user.user_group.name == "Project Leader"
+      can :read, [Labor, Position, MaterialAdjustment, Log] if user.user_group.name == "Project Leader"
       
       can [:edit_profile, :update_profile], User if user.user_group.name == "Project Leader"
       
       can :manage, [Warehouse, Material, PlantingProject, Machinery, MachineryType, EquipmentType, Equipment, WarehouseProductionAmount, WarehouseMaterialReceived, StockIn, WarehouseItemTransaction, InputTask, OutputTask, Farm, Block] if user.user_group.name == "Project Leader"
       
-      can :create, [PlanFarm, ProductionPlan] if user.user_group.name == "Project Leader"
+      can [:read, :create], [PlanFarm, ProductionPlan, ProductionStandard] if user.user_group.name == "Project Leader"
+
+      can :index, :production_plan_report if user.user_group.name == "Project Leader"
 
       cannot :index, :production_list if user.user_group.name == "Project Leader"
 
@@ -36,13 +40,15 @@ class Ability
 
       
       # =================== Data Entry =======================
-      can :read, WarehouseMaterialReceived if user.user_group.name == "Data Entry"
-
-      can [:read, :create], [Labor, Position, MachineryType, Machinery, EquipmentType, Equipment, StockIn, WarehouseItemTransaction, InputTask, OutputTask, PlanFarm, ProductionPlan] if user.user_group.name == "Data Entry"
+      can [:read, :create], [Labor, Position, MachineryType, Machinery, EquipmentType, Equipment, StockIn, WarehouseItemTransaction, InputTask, OutputTask, PlanFarm, ProductionPlan, ProductionStandard] if user.user_group.name == "Data Entry"
 
       can :manage, WarehouseMaterialReceived if user.user_group.name == "Data Entry"
+
       can [:edit_profile, :update_profile], User if user.user_group.name == "Data Entry"
-      cannot :index, :production_list if user.user_group.name == "Data Entry"
+
+      cannot :index, [:production_list, :production_plan_report] if user.user_group.name == "Data Entry"
+
+
 
       
 
