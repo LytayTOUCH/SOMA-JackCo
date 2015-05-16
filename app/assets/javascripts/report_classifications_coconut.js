1e8a9f8942d046cba2fd2131480bedc296464110
Reaksmey <r.reaksmey@gmail.com>
2015-05-16 18:45:07 +0700

    Finish printing feature for all my reports

--- app/assets/javascripts/report_classifications_coconut.js
+++ app/assets/javascripts/report_classifications_coconut.js
@@ -1,5 +1,11 @@
+$(document).ready(function() {
+  $('#print_button').click(function() { window.print(); });
+});
+
 google.load('visualization', '1', {packages: ['corechart', 'line']});
 google.setOnLoadCallback(drawBasic);
+
+var year = $("#year").val() == undefined? "" : $("#year").val();
 
 var standard_pdt = JSON.parse($("#standard_pdt").val() == undefined? "[0,0,0,0,0,0,0,0,0,0,0,0]" : $("#standard_pdt").val());
 var forecast_pdt = JSON.parse($("#forecast_pdt").val() == undefined? "[0,0,0,0,0,0,0,0,0,0,0,0]" : $("#forecast_pdt").val());
@@ -21,18 +27,18 @@ function drawBasic() {
   data.addColumn('number', 'Free at Farm');
 
   data.addRows([
-    ["Jan-15", standard_pdt[0],  forecast_pdt[0],  actual_pdt[0],  to_finish_wh[0],  to_nursery_wh[0],  spoiled[0],  free_at_farm[0]],
-    ["Feb-15", standard_pdt[1],  forecast_pdt[1],  actual_pdt[1],  to_finish_wh[1],  to_nursery_wh[1],  spoiled[1],  free_at_farm[1]],
-    ["Mar-15", standard_pdt[2],  forecast_pdt[2],  actual_pdt[2],  to_finish_wh[2],  to_nursery_wh[2],  spoiled[2],  free_at_farm[2]],
-    ["Apr-15", standard_pdt[3],  forecast_pdt[3],  actual_pdt[3],  to_finish_wh[3],  to_nursery_wh[3],  spoiled[3],  free_at_farm[3]],
-    ["May-15", standard_pdt[4],  forecast_pdt[4],  actual_pdt[4],  to_finish_wh[4],  to_nursery_wh[4],  spoiled[4],  free_at_farm[4]],
-    ["Jun-15", standard_pdt[5],  forecast_pdt[5],  actual_pdt[5],  to_finish_wh[5],  to_nursery_wh[5],  spoiled[5],  free_at_farm[5]],
-    ["Jul-15", standard_pdt[6],  forecast_pdt[6],  actual_pdt[6],  to_finish_wh[6],  to_nursery_wh[6],  spoiled[6],  free_at_farm[6]],
-    ["Aug-15", standard_pdt[7],  forecast_pdt[7],  actual_pdt[7],  to_finish_wh[7],  to_nursery_wh[7],  spoiled[7],  free_at_farm[7]],
-    ["Sep-15", standard_pdt[8],  forecast_pdt[8],  actual_pdt[8],  to_finish_wh[8],  to_nursery_wh[8],  spoiled[8],  free_at_farm[8]],
-    ["Oct-15", standard_pdt[9],  forecast_pdt[9],  actual_pdt[9],  to_finish_wh[9],  to_nursery_wh[9],  spoiled[9],  free_at_farm[9]],
-    ["Nov-15", standard_pdt[10], forecast_pdt[10], actual_pdt[10], to_finish_wh[10], to_nursery_wh[10], spoiled[10], free_at_farm[10]],
-    ["Dec-15", standard_pdt[11], forecast_pdt[11], actual_pdt[11], to_finish_wh[11], to_nursery_wh[11], spoiled[11], free_at_farm[11]]
+    ["Jan-"+year, standard_pdt[0],  forecast_pdt[0],  actual_pdt[0],  to_finish_wh[0],  to_nursery_wh[0],  spoiled[0],  free_at_farm[0]],
+    ["Feb-"+year, standard_pdt[1],  forecast_pdt[1],  actual_pdt[1],  to_finish_wh[1],  to_nursery_wh[1],  spoiled[1],  free_at_farm[1]],
+    ["Mar-"+year, standard_pdt[2],  forecast_pdt[2],  actual_pdt[2],  to_finish_wh[2],  to_nursery_wh[2],  spoiled[2],  free_at_farm[2]],
+    ["Apr-"+year, standard_pdt[3],  forecast_pdt[3],  actual_pdt[3],  to_finish_wh[3],  to_nursery_wh[3],  spoiled[3],  free_at_farm[3]],
+    ["May-"+year, standard_pdt[4],  forecast_pdt[4],  actual_pdt[4],  to_finish_wh[4],  to_nursery_wh[4],  spoiled[4],  free_at_farm[4]],
+    ["Jun-"+year, standard_pdt[5],  forecast_pdt[5],  actual_pdt[5],  to_finish_wh[5],  to_nursery_wh[5],  spoiled[5],  free_at_farm[5]],
+    ["Jul-"+year, standard_pdt[6],  forecast_pdt[6],  actual_pdt[6],  to_finish_wh[6],  to_nursery_wh[6],  spoiled[6],  free_at_farm[6]],
+    ["Aug-"+year, standard_pdt[7],  forecast_pdt[7],  actual_pdt[7],  to_finish_wh[7],  to_nursery_wh[7],  spoiled[7],  free_at_farm[7]],
+    ["Sep-"+year, standard_pdt[8],  forecast_pdt[8],  actual_pdt[8],  to_finish_wh[8],  to_nursery_wh[8],  spoiled[8],  free_at_farm[8]],
+    ["Oct-"+year, standard_pdt[9],  forecast_pdt[9],  actual_pdt[9],  to_finish_wh[9],  to_nursery_wh[9],  spoiled[9],  free_at_farm[9]],
+    ["Nov-"+year, standard_pdt[10], forecast_pdt[10], actual_pdt[10], to_finish_wh[10], to_nursery_wh[10], spoiled[10], free_at_farm[10]],
+    ["Dec-"+year, standard_pdt[11], forecast_pdt[11], actual_pdt[11], to_finish_wh[11], to_nursery_wh[11], spoiled[11], free_at_farm[11]]
   ]);
 
   var options = {
--- app/assets/javascripts/report_classifications_jackfruit.js
+++ app/assets/javascripts/report_classifications_jackfruit.js
@@ -1,5 +1,11 @@
+$(document).ready(function() {
+  $('#print_button').click(function() { window.print(); });
+});
+
 google.load('visualization', '1', {packages: ['corechart', 'line']});
 google.setOnLoadCallback(drawBasic);
+
+var year = $("#year").val() == undefined? "" : $("#year").val();
 
 var standard_pdt = JSON.parse($("#standard_pdt").val() == undefined? "[0,0,0,0,0,0,0,0,0,0,0,0]" : $("#standard_pdt").val());
 var forecast_pdt = JSON.parse($("#forecast_pdt").val() == undefined? "[0,0,0,0,0,0,0,0,0,0,0,0]" : $("#forecast_pdt").val());
@@ -21,18 +27,18 @@ function drawBasic() {
   data.addColumn('number', 'Spoiled Ripe');
     
   data.addRows([
-    ["Jan-15", standard_pdt[0],  forecast_pdt[0],  actual_pdt[0],  to_finish_wh[0],  to_nursery_wh[0],  young_fruit[0],  spoiled_ripe[0]],
-    ["Feb-15", standard_pdt[1],  forecast_pdt[1],  actual_pdt[1],  to_finish_wh[1],  to_nursery_wh[1],  young_fruit[1],  spoiled_ripe[1]],
-    ["Mar-15", standard_pdt[2],  forecast_pdt[2],  actual_pdt[2],  to_finish_wh[2],  to_nursery_wh[2],  young_fruit[2],  spoiled_ripe[2]],
-    ["Apr-15", standard_pdt[3],  forecast_pdt[3],  actual_pdt[3],  to_finish_wh[3],  to_nursery_wh[3],  young_fruit[3],  spoiled_ripe[3]],
-    ["May-15", standard_pdt[4],  forecast_pdt[4],  actual_pdt[4],  to_finish_wh[4],  to_nursery_wh[4],  young_fruit[4],  spoiled_ripe[4]],
-    ["Jun-15", standard_pdt[5],  forecast_pdt[5],  actual_pdt[5],  to_finish_wh[5],  to_nursery_wh[5],  young_fruit[5],  spoiled_ripe[5]],
-    ["Jul-15", standard_pdt[6],  forecast_pdt[6],  actual_pdt[6],  to_finish_wh[6],  to_nursery_wh[6],  young_fruit[6],  spoiled_ripe[6]],
-    ["Aug-15", standard_pdt[7],  forecast_pdt[7],  actual_pdt[7],  to_finish_wh[7],  to_nursery_wh[7],  young_fruit[7],  spoiled_ripe[7]],
-    ["Sep-15", standard_pdt[8],  forecast_pdt[8],  actual_pdt[8],  to_finish_wh[8],  to_nursery_wh[8],  young_fruit[8],  spoiled_ripe[8]],
-    ["Oct-15", standard_pdt[9],  forecast_pdt[9],  actual_pdt[9],  to_finish_wh[9],  to_nursery_wh[9],  young_fruit[9],  spoiled_ripe[9]],
-    ["Nov-15", standard_pdt[10], forecast_pdt[10], actual_pdt[10], to_finish_wh[10], to_nursery_wh[10], young_fruit[10], spoiled_ripe[10]],
-    ["Dec-15", standard_pdt[11], forecast_pdt[11], actual_pdt[11], to_finish_wh[11], to_nursery_wh[11], young_fruit[11], spoiled_ripe[11]]
+    ["Jan-"+year, standard_pdt[0],  forecast_pdt[0],  actual_pdt[0],  to_finish_wh[0],  to_nursery_wh[0],  young_fruit[0],  spoiled_ripe[0]],
+    ["Feb-"+year, standard_pdt[1],  forecast_pdt[1],  actual_pdt[1],  to_finish_wh[1],  to_nursery_wh[1],  young_fruit[1],  spoiled_ripe[1]],
+    ["Mar-"+year, standard_pdt[2],  forecast_pdt[2],  actual_pdt[2],  to_finish_wh[2],  to_nursery_wh[2],  young_fruit[2],  spoiled_ripe[2]],
+    ["Apr-"+year, standard_pdt[3],  forecast_pdt[3],  actual_pdt[3],  to_finish_wh[3],  to_nursery_wh[3],  young_fruit[3],  spoiled_ripe[3]],
+    ["May-"+year, standard_pdt[4],  forecast_pdt[4],  actual_pdt[4],  to_finish_wh[4],  to_nursery_wh[4],  young_fruit[4],  spoiled_ripe[4]],
+    ["Jun-"+year, standard_pdt[5],  forecast_pdt[5],  actual_pdt[5],  to_finish_wh[5],  to_nursery_wh[5],  young_fruit[5],  spoiled_ripe[5]],
+    ["Jul-"+year, standard_pdt[6],  forecast_pdt[6],  actual_pdt[6],  to_finish_wh[6],  to_nursery_wh[6],  young_fruit[6],  spoiled_ripe[6]],
+    ["Aug-"+year, standard_pdt[7],  forecast_pdt[7],  actual_pdt[7],  to_finish_wh[7],  to_nursery_wh[7],  young_fruit[7],  spoiled_ripe[7]],
+    ["Sep-"+year, standard_pdt[8],  forecast_pdt[8],  actual_pdt[8],  to_finish_wh[8],  to_nursery_wh[8],  young_fruit[8],  spoiled_ripe[8]],
+    ["Oct-"+year, standard_pdt[9],  forecast_pdt[9],  actual_pdt[9],  to_finish_wh[9],  to_nursery_wh[9],  young_fruit[9],  spoiled_ripe[9]],
+    ["Nov-"+year, standard_pdt[10], forecast_pdt[10], actual_pdt[10], to_finish_wh[10], to_nursery_wh[10], young_fruit[10], spoiled_ripe[10]],
+    ["Dec-"+year, standard_pdt[11], forecast_pdt[11], actual_pdt[11], to_finish_wh[11], to_nursery_wh[11], young_fruit[11], spoiled_ripe[11]]
   ]);
    
   var options = {
