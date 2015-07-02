9b722735261ee4ab1f1745466ed2eb6f88e70683
Teo Paocheak <teopaocheak@yahoo.com>
2015-07-02 11:14:03 +0700

    refactor - remove Labor & LaborProject models

--- app/models/labor_project.rb
+++
@@ -1,5 +0,0 @@
-class LaborProject < ActiveRecord::Base
-  belongs_to :labor, foreign_key: :labor_uuid
-  belongs_to :project, foreign_key: :project_uuid
-  
-end
--- app/models/project.rb
+++
@@ -1,9 +0,0 @@
-class Project < ActiveRecord::Base
-  include UuidHelper
-
-  validates :name, length: { maximum: 50 }, :presence => { message: "Project is required." }
-  
-  has_many :labor_projects, dependent: :destroy
-  has_many :labors, through: :labor_projects
-
-end
